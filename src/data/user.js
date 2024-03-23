d413536ff0c21a67ca79d2a61962c89d00c80e68
Animesh239 <animeshbarik239@gmail.com>
2024-03-23 14:38:16 +0530

    Add init

---
+++ src/data/user.js
@@ -0,0 +1,80 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by animesh239",
+		name: "Animesh Barik.",
+		email: "animeshbarik239@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/Animesh239",
+		linkedin: "https://linkedin.com/in/animesh239",
+	},
+
+	homepage: {
+		title: "Full-stack web developer and programmer, and an amateur reader.",
+		description:
+			"I am a full-stack developer with expertise in React.js and Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Animesh Barik. I live India, where I try to rebuild the future.",
+		description:
+			"I am a undergraduate student at the National Institute of Technology, Rourkela. I am passionate about technology and innovation. I love to build things that make a difference. I am always looking for new challenges and opportunities to learn and grow. I am a self-taught programmer and I have experience in building web applications using various technologies. I am proficient in JavaScript, React.js, Node.js, and MongoDB. I am always looking for new opportunities to work on exciting projects and collaborate with other developers.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "NestONDC e-commerce website",
+			description:
+				" Our project is an ONDC (Open Network for Digital Commerce) site integrated with OpenStreetMap. Leveraging the OpenStreetMap API for location-based services, this platform visualizes shop locations on a high-definition geo map",
+			logo: "https://res.cloudinary.com/dh4qhdszo/image/upload/c_scale,h_800,w_1000/reactfolio/ksckrjvtbu8xrjnuste1.jpg",
+			linkText: "View Project",
+			link: "https://e-commerce-website-rho-six.vercel.app/",
+		},
+
+		{
+			title: "Wordle Game",
+			description:
+				"A 5-letter word guessing game, similar to Wordle, using React. Players guess a word in five tries, with color-coded feedback.",
+			logo: "https://tse3.mm.bing.net/th?id=OIP.DDMqzbkpBck86PHJ0GqAQwAAAA&pid=Api&P=0&h=180",
+			linkText: "View Project",
+			link: "https://animesh239.github.io/game_gta/",
+		},
+
+		{
+			title: "Minare",
+			description:
+				"This project is engineered to develop the website of MINARE’24, one of the largest Geo-Mining Fest hosted by Mining Engg. Society of NIT, Rourkela.",
+			logo: "https://res.cloudinary.com/dh4qhdszo/image/upload/b_rgb:000000/reactfolio/cnh0p6jqhfkkoc027izf.jpg",
+			linkText: "View Project",
+			link: "https://minare.nitrkl.ac.in/",
+		},
+
+		{
+			title: "Dalchini",
+			description:
+				"An advanced note taking app, with features like rich text editing, markdown support, and more. Built using React.js, GraphQL and Firebase. It is still in development.",
+			logo: "https://cdn-icons-png.flaticon.com/128/8521/8521476.png",
+			linkText: "View Project",
+			link: "https://github.com/Animesh239/dalchini",
+		},
+
+		{
+			title: "Genie",
+			description:
+				"Simplify your reading with Summarize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries",
+			logo: "https://resplendent-sawine-1030db.netlify.app/assets/genie-logo2-1816478c.png",
+			linkText: "View Project",
+			link: "https://resplendent-sawine-1030db.netlify.app/",
+		},
+	],
+};
+
+export default INFO;
